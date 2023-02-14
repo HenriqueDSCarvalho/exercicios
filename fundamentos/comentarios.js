436416354f350238a9a703c73049c809079ab030
32265256Hen@ <h96352828@gmail.com>
2023-02-14 12:45:22 -0300

    first commit

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+//comentario de uma linha
+console.log('linha 1')
+
+/*
+comentario de 
+varias linhas
+*/
+console.log('linha 2')
+
+/*
+*comentario 
+*de varias linhas
+*identado
+*/
+console.log('linha 3')
---
+++ fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log('ola mundo');
+
+{
+    {
+        console.log("olá");
+        console.log('mundo') //padrao do curso
+    }
+}
